7b931dd138be7a83fefa04b6e03c60e3cc167b85
Kaede Hoshikawa <futursolo@icloud.com>
2021-12-12 09:57:36 +0900

    Add GitHub Actions.

--- packages/bounce/src/lib.rs
+++ packages/bounce/src/lib.rs
@@ -1,3 +1,11 @@
+#![deny(clippy::all)]
+#![deny(missing_debug_implementations)]
+#![deny(unsafe_code)]
+#![deny(non_snake_case)]
+#![deny(clippy::cognitive_complexity)]
+#![cfg_attr(documenting, feature(doc_cfg))]
+#![cfg_attr(any(releasing, not(debug_assertions)), deny(dead_code, unused_imports))]
+
 mod atom;
 mod hooks;
 mod provider;
