c90dfc6def19b26a5997489970e9ab64afad536f
Kaede Hoshikawa <futursolo@icloud.com>
2022-03-03 21:54:29 +0900

    Update docs and changelog.

--- crates/bounce/src/states/observer.rs
+++ crates/bounce/src/states/observer.rs
@@ -4,7 +4,25 @@ use std::rc::Rc;
 ///
 /// Currently, only Slices and Atoms can be observed. This API may be expanded to other state types
 /// in the future.
+///
+/// # Example
+///
+/// ```
+/// use bounce::prelude::*;
+///
+/// #[derive(Atom, PartialEq, Default)]
+/// #[observed] // observed states need to be denoted with the observed attribute.
+/// struct State {
+///     value: usize,
+/// }
+///
+/// impl Observed for State {
+///     fn changed(self: Rc<Self>) {
+///         // this method will be called when the value of the state changes.
+///     }
+/// }
+/// ```
 pub trait Observed {
     /// Notified when the state value has changed.
-    fn changed(self: Rc<Self>) {}
+    fn changed(self: Rc<Self>);
 }
